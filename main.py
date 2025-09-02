6cbd9ad9df2a5643fe60d2e3dc12fd8a43755f1a
sohan <mail.sohansprabhu@gmail.com>
2025-09-02 12:25:21 +0100

    setting up weights and layers in ipynb

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import numpy as np
-import matplotlib.pyplot as np
+import matplotlib.pyplot as plt
 import pandas as pd
 
 
 
-df = pd.read_csv('train.csv') # loading the csv file
+df = pd.read_csv('o.csv') # loading the csv file
 
 trainData = df.to_numpy() # converting the dataframe into a numpy array
+
+# setting up the label
+imgd = trainData[0][1:].reshape(28,28)
+imgd = np.rot90(np.flip(imgd,1))
+lbl = trainData[0][0]
+
