cf6dfba9d00eb9928e5ace2229c9b2863a3b57f3
sohan <mail.sohansprabhu@gmail.com>
2025-09-09 22:53:36 +0100

    Gradient Descent with Training implemented successfully

--- Network.py
+++ Network.py
@@ -43,6 +43,25 @@ class NeuralNetwork:
                 self.LayerBias_dict[f"output_0_bias"] = np.zeros((self.outputs,1))
                 self.LayerWights_dict[f"output_0_weight"] = np.random.normal(0.0,0.5,(self.outputs,self.hiddenLayerNodes,))
 
+        # setting up the stages manually *will implement automatic generation in future
+
+
+
+
+    def forwardPropogate(self,Input):
+
+        # forward propogation of Network manually *will implement automatic generation in future
+
+        self.Z0 = (self.LayerWights_dict["hidden_0_weight"]@Input) + self.LayerBias_dict["hidden_0_bias"]
+        self.A0 = sig(self.Z0)
+        self.Z1 = (self.LayerWights_dict["hidden_1_weight"]@self.A0) + self.LayerBias_dict["hidden_1_bias"]
+        self.A1 = sig(self.Z1)
+        self.Z2 = (self.LayerWights_dict["output_0_weight"]@self.A1) + self.LayerBias_dict["output_0_bias"]
+        self.A2 = sig(self.Z2)
+
+        return self.A2
+        
+
 
 # defining the sum of square residuals to calculate cost
 def SSR(o , x ):
@@ -58,19 +77,19 @@ def sig(M , derivative = False):
        
 
 
-def Manual_BackProp(Z0,A0,Z1,A1,Z2,A2,Test,Input,Label):
-    #storing manual backpropogation gradients That will be returned
+def Manual_BackProp(Test,Input,Label):
+    #storing manual backpropogation gradients That will be returned  *will implement automatic generation in future
     Gradients = {}
     
 
 
     # calculating garadients of last layer weights and biases
 
-    dC_dA2 = 2*(A2 - Label)
-    dA2_dZ2 = sig(Z2,derivative=True)
+    dC_dA2 = 2*(Test.A2 - Label)
+    dA2_dZ2 = sig(Test.Z2,derivative=True)
     S2 = np.multiply(dC_dA2,dA2_dZ2)
 
-    dC_dW2 = S2@(A1.T)
+    dC_dW2 = S2@(Test.A1.T)
     dC_dB2 = S2
 
     #appending the gradients
@@ -85,10 +104,10 @@ def Manual_BackProp(Z0,A0,Z1,A1,Z2,A2,Test,Input,Label):
     # calculating garadients of 2nd to last layer weights and biases
     W2 = Test.LayerWights_dict["output_0_weight"]
     dC_dA1 = (W2.T)@S2
-    dA1_dZ1 = sig(Z1,derivative=True)
+    dA1_dZ1 = sig(Test.Z1,derivative=True)
     S1 = np.multiply(dC_dA1,dA1_dZ1)
 
-    dC_dW1 = S1@(A0.T)
+    dC_dW1 = S1@(Test.A0.T)
     dC_dB1 = S1
 
     #appending the gradients
@@ -103,7 +122,7 @@ def Manual_BackProp(Z0,A0,Z1,A1,Z2,A2,Test,Input,Label):
     # calculating garadients of 3rd to last layer weights and biases
     W1 = Test.LayerWights_dict["hidden_1_weight"]
     dC_dA0 = (W1.T)@S1
-    dA0_dZ0 = sig(Z0,derivative=True)
+    dA0_dZ0 = sig(Test.Z0,derivative=True)
     S0 = np.multiply(dC_dA0,dA0_dZ0)
 
     dC_dW0 = S0@(Input.T)
@@ -120,3 +139,29 @@ def Manual_BackProp(Z0,A0,Z1,A1,Z2,A2,Test,Input,Label):
     #print("___")
 
     return Gradients
+
+def SumGradients(CurrGrad , NewGrad):
+    # manually summing the gradients for the minibatch
+    CurrGrad["hidden_0_weight_gradients"] += NewGrad["hidden_0_weight_gradients"]
+    CurrGrad["hidden_0_bias_gradients"] += NewGrad["hidden_0_bias_gradients"]
+
+    CurrGrad["hidden_1_weight_gradients"] += NewGrad["hidden_1_weight_gradients"]
+    CurrGrad["hidden_1_bias_gradients"] += NewGrad["hidden_1_bias_gradients"]
+
+    CurrGrad["output_0_weight_gradients"] += NewGrad["output_0_weight_gradients"]
+    CurrGrad["output_0_bias_gradients"] += NewGrad["output_0_bias_gradients"]
+
+    return CurrGrad
+
+def GradientUpdate(Test , Gradients , n):
+
+    # manually adjusting gradients for weights and biases
+
+    Test.LayerWights_dict["hidden_0_weight"] = Test.LayerWights_dict["hidden_0_weight"] - n*(Gradients["hidden_0_weight_gradients"])
+    Test.LayerBias_dict["hidden_0_bias"] = Test.LayerBias_dict["hidden_0_bias"] - n*(Gradients["hidden_0_bias_gradients"])
+
+    Test.LayerWights_dict["hidden_1_weight"] = Test.LayerWights_dict["hidden_1_weight"] - n*(Gradients["hidden_1_weight_gradients"])
+    Test.LayerBias_dict["hidden_1_bias"] = Test.LayerBias_dict["hidden_1_bias"] - n*(Gradients["hidden_1_bias_gradients"])
+
+    Test.LayerWights_dict["output_0_weight"] = Test.LayerWights_dict["output_0_weight"] - n*(Gradients["output_0_weight_gradients"])
+    Test.LayerBias_dict["output_0_bias"] = Test.LayerBias_dict["output_0_bias"] - n*(Gradients["output_0_bias_gradients"])
--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import Network
-from Network import sig , SSR ,Manual_BackProp
+from Network import sig , SSR ,Manual_BackProp , GradientUpdate , SumGradients
 # setting numpy seed to ensure same results
 np.random.seed(0) 
 
-df = pd.read_csv('o.csv') # loading the csv file
+df = pd.read_csv('train.csv') # loading the csv file
 
 trainData = df.to_numpy() # converting the dataframe into a numpy array
-
+print(trainData.shape)
 # setting up the label
 imgd = trainData[0][1:].reshape(28,28)
 imgd = np.rot90(np.flip(imgd,1))
@@ -25,80 +25,35 @@ Test = Network.NeuralNetwork(784 , 2 ,16 , 10 )
 
 # forward propogation of Network
 
-Z0 = (Test.LayerWights_dict["hidden_0_weight"]@Input) + Test.LayerBias_dict["hidden_0_bias"]
-A0 = sig(Z0)
-Z1 = (Test.LayerWights_dict["hidden_1_weight"]@A0) + Test.LayerBias_dict["hidden_1_bias"]
-A1 = sig(Z1)
-Z2 = (Test.LayerWights_dict["output_0_weight"]@A1) + Test.LayerBias_dict["output_0_bias"]
-A2 = sig(Z2)
+
+A2 = Test.forwardPropogate(Input)
 C = SSR(A2,Label)
+n = 0.001  #learning rate or step size for gradient descent
+Gradients = None
+count = 1
+batch_size = 100
+for i in range(10000):
 
+    for j in range(batch_size):
+        Input = trainData[count][1:].reshape(784,1)
+        Label = np.zeros((10,1))
+        lbl = trainData[count][0]
+        Label[lbl][0] = 1
 
-print(C)
+        A2 = Test.forwardPropogate(Input)
+        C = SSR(A2,Label)
+        
+        if Gradients !=None:
+            Gradients = SumGradients(Gradients, Manual_BackProp(Test,Input,Label))
+        else:
+            Gradients = Manual_BackProp(Test,Input,Label)
+        count+=1
+        if count ==239999:
+            count = 0
+        
+        
 
-
-# Manual Backpropogation as a Test
-
-# calculating garadients of last layer weights and biases
-
-dC_dA2 = 2*(A2 - Label)
-dA2_dZ2 = sig(Z2,derivative=True)
-S2 = np.multiply(dC_dA2,dA2_dZ2)
-
-dC_dW2 = S2@(A1.T)
-dC_dB2 = S2
-print("___")
-print(dC_dW2.shape , "dC_dW2")
-print(dC_dB2.shape , "dC_dB2")
-print("___")
-
-# calculating garadients of 2nd to last layer weights and biases
-W2 = Test.LayerWights_dict["output_0_weight"]
-dC_dA1 = (W2.T)@S2
-dA1_dZ1 = sig(Z1,derivative=True)
-S1 = np.multiply(dC_dA1,dA1_dZ1)
-
-dC_dW1 = S1@(A0.T)
-dC_dB1 = S1
-print("___")
-print(dC_dW1.shape , "dC_dW1")
-print(dC_dB1.shape , "dC_dB1")
-print("___")
-
-# calculating garadients of 3rd to last layer weights and biases
-W1 = Test.LayerWights_dict["hidden_1_weight"]
-dC_dA0 = (W1.T)@S1
-dA0_dZ0 = sig(Z0,derivative=True)
-S0 = np.multiply(dC_dA0,dA0_dZ0)
-
-dC_dW0 = S0@(Input.T)
-dC_dB0 = S0
-print("___")
-print(dC_dW0.shape , "dC_dW0")
-print(dC_dB0.shape , "dC_dB0")
-print("___")
-for i in range(100):
-    Gradients = Manual_BackProp(Z0,A0,Z1,A1,Z2,A2,Test,Input,Label)
-
-    n = 0.1 # learning rate or step size for gradient descent
-
-    # manually adjusting gradients for weights and biases
-
-    Test.LayerWights_dict["hidden_0_weight"] = Test.LayerWights_dict["hidden_0_weight"] - n*(Gradients["hidden_0_weight_gradients"])
-    Test.LayerBias_dict["hidden_0_bias"] = Test.LayerBias_dict["hidden_0_bias"] - n*(Gradients["hidden_0_bias_gradients"])
-
-    Test.LayerWights_dict["hidden_1_weight"] = Test.LayerWights_dict["hidden_1_weight"] - n*(Gradients["hidden_1_weight_gradients"])
-    Test.LayerBias_dict["hidden_1_bias"] = Test.LayerBias_dict["hidden_1_bias"] - n*(Gradients["hidden_1_bias_gradients"])
-
-    Test.LayerWights_dict["output_0_weight"] = Test.LayerWights_dict["output_0_weight"] - n*(Gradients["output_0_weight_gradients"])
-    Test.LayerBias_dict["output_0_bias"] = Test.LayerBias_dict["output_0_bias"] - n*(Gradients["output_0_bias_gradients"])
-
-    Z0 = (Test.LayerWights_dict["hidden_0_weight"]@Input) + Test.LayerBias_dict["hidden_0_bias"]
-    A0 = sig(Z0)
-    Z1 = (Test.LayerWights_dict["hidden_1_weight"]@A0) + Test.LayerBias_dict["hidden_1_bias"]
-    A1 = sig(Z1)
-    Z2 = (Test.LayerWights_dict["output_0_weight"]@A1) + Test.LayerBias_dict["output_0_bias"]
-    A2 = sig(Z2)
-    C = SSR(A2,Label)
-
-    print(C)
+    GradientUpdate(Test , Gradients , n/batch_size)
+    Gradients = None
+    print(C , f"--- count {count}")
+        
